f16f548222db5c28937709bf0fa5f87d697aa261
Wiktor Stankiewicz <wiktorstankiewicz24@gmail.com>
2021-12-07 10:12:25 +0100

    Wyswietlanie pustej ramki i panelu

--- project/src/Grafika/Ramka.java
+++ project/src/Grafika/Ramka.java
@@ -6,6 +6,10 @@ import java.awt.event.ActionListener;
 public class Ramka extends JFrame {
     public Ramka(){
         this.add(new Panel());
+        this.pack();
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
     }
 
 }
