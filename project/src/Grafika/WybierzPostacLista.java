e6351250fe7dd47aee84f5413117f52023863039
DudasOn <pawel200216@interia.eu>
2021-12-07 10:51:09 +0100

    Dodanie listy wyboru postaci

--- project/src/Grafika/WybierzPostacLista.java
+++ project/src/Grafika/WybierzPostacLista.java
@@ -1,9 +1,47 @@
 package Grafika;
 
+import Postac.*;
+
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class WybierzPostacLista extends JList <Object> {
 
-    /*Reflections reflections = new Reflections("com.mycompany");
-    Set<Class<? extends MyInterface>> classes = reflections.getSubTypesOf(MyInterface.class);*/
+public class WybierzPostacLista implements ListSelectionListener {
+
+    String[] listaKlas = {"Wojownik", "Zabójca", "Łowca", "Mag"};
+
+    JList<?> lista = new JList<>(listaKlas);
+
+    JScrollPane przewijanie = new JScrollPane(lista);
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame();
+
+    public void dzialaj() {
+
+        przewijanie.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        przewijanie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        frame.setSize(300, 300);
+        frame.add(panel);
+        frame.setVisible(true);
+        panel.setVisible(true);
+        panel.add(przewijanie);
+        lista.setVisibleRowCount(4);
+        lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        lista.addListSelectionListener(this);
+
+
+    }
+
+
+    @Override
+    public void valueChanged(ListSelectionEvent zaznaczenie) {
+        if (!zaznaczenie.getValueIsAdjusting()) {
+            String wybor = (String) lista.getSelectedValue();
+
+            System.out.println(wybor);
+        }
+
+    }
 }
