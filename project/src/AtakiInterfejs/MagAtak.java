8eff596fa0cc704b0a3952296836a44426bcd11f
Tinczo <doszyoma@gmail.com>
2021-12-07 14:38:28 +0100

    Stworzenie Interfejsow Ataku i sposobow uniku

---
+++ project/src/AtakiInterfejs/MagAtak.java
@@ -0,0 +1,15 @@
+package AtakiInterfejs;
+import Postacie.*;
+
+public class MagAtak implements AtakiInterfejs_I{
+    @Override
+    public void Atak(Postacie P) {
+        int ArmorTMP = P.getPrzeciwnik().getArmor();
+        int DodgeTMP = P.getPrzeciwnik().getDodgeChance();
+        P.getPrzeciwnik().setArmor(0);
+        P.getPrzeciwnik().setDodgeChance(-1);
+        P.ZadajObrazenia();
+        P.getPrzeciwnik().setArmor(ArmorTMP);
+        P.getPrzeciwnik().setDodgeChance(DodgeTMP);
+    }
+}
