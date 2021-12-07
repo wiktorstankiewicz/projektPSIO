71da4b0685f905a52499dc768fe0f9cde1823ad5
Michał <michalzadelek10@gmail.com>
2021-12-07 12:55:09 +0100

    Test czy dziala git

--- project/src/Main.java
+++ project/src/Main.java
@@ -2,6 +2,7 @@ import Grafika.Ramka;
 
 public class Main {
     private int cos  = 0;
+    private int test;
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Ramka ramka = new Ramka();
