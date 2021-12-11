c06bcb62725047a2e5275a2549d1b052bc6a75b7
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-12-12 00:26:15 +0100

    Create Launcher base and update gitignore

--- project/src/Launcher.java
+++ project/src/Launcher.java
@@ -1,6 +1,12 @@
+import PostaciCreator.Controller.Controller;
+import PostaciCreator.Controller.ControllerInterface;
+import PostaciCreator.Model.Model;
+import PostaciCreator.Model.ModelInterface;
+
 public class Launcher {
 
 	public static void main(String[] args){
-
+		ModelInterface model = new Model();
+		ControllerInterface controller = new Controller(model);
 	}
 }
