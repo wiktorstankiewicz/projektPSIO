6aa74f5882d0777b1f9feac6d52a6afb3c7020f3
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-01-19 22:57:30 +0100

    Rozpocznij prace nad testami atakow

--- project/test/testGra/testModel/testAtakiInterfejs/TestAtaki.java
+++ project/test/testGra/testModel/testAtakiInterfejs/TestAtaki.java
@@ -1,4 +1,58 @@
 package testGra.testModel.testAtakiInterfejs;
 
+import gra.model.bron.lowcy.Luk;
+import gra.model.bron.maga.Rozdzka;
+import gra.model.bron.woja.Miecz;
+import gra.model.bron.woja.Mlot;
+import gra.model.bron.zabojcy.Sztylet;
+import gra.model.postacie.Postac;
+import gra.model.postacie.dystansowe.Lowca;
+import gra.model.postacie.dystansowe.Mag;
+import gra.model.postacie.wZwarciu.Wojownik;
+import gra.model.postacie.wZwarciu.Zabojca;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestAtaki {
+	private static Postac lowca;
+	private static Postac mag;
+	private static Postac wojownikMiecz;
+	private static Postac wojownikMlot;
+	private static Postac zabojca;
+
+	@BeforeClass
+	public static void setup(){
+		lowca = new Lowca("Lowca", new Luk());
+		mag = new Mag("Mag", new Rozdzka());
+		wojownikMiecz = new Wojownik("Wojownik Miecz", new Miecz());
+		wojownikMlot = new Wojownik("Wojownik Mlot", new Mlot());
+		zabojca = new Zabojca("Zabojca", new Sztylet());
+	}
+
+	@Test
+	public void testAtakiLowca(){
+
+	}
+
+	@Test
+	public void testAtakiMag(){
+
+	}
+
+	@Test
+	public void testAtakiWojownikMiecz(){
+
+	}
+
+	@Test
+	public void testAtakiWojownikMlot(){
+
+	}
+
+	@Test
+	public void testAtakiZabojca(){
+
+	}
+
+
 }
